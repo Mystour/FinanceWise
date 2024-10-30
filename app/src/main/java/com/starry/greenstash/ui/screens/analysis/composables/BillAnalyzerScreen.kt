e5a8a2df315316604c115658eac5555006ab7ef7
陈子昂 <a960015420@163.com>
2024-10-30 22:52:58 +0800

    refactor(ui): 移除未使用的导入语句
    - 删除了 BillAnalyzerScreen.kt 文件中的未使用导入语句
    - 优化了代码结构，提高了代码的可读性和维护性

--- app/src/main/java/com/starry/greenstash/ui/screens/analysis/composables/BillAnalyzerScreen.kt
+++ app/src/main/java/com/starry/greenstash/ui/screens/analysis/composables/BillAnalyzerScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
