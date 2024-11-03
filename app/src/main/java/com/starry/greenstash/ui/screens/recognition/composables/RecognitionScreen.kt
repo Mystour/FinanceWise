c25e82900b9e4f47e3060648cf5327f6e0f6a83b
陈子昂 <a960015420@163.com>
2024-11-03 20:35:06 +0800

    refactor(ui): 移除未使用的导入语句
    - 在 EmotionScreen.kt 中移除了未使用的 AnnotatedString 导入- 在 RecognitionScreen.kt 中移除了未使用的 buildAnnotatedString、EmotionViewModel 和 EmotionViewModelFactory 导入

--- app/src/main/java/com/starry/greenstash/ui/screens/recognition/composables/RecognitionScreen.kt
+++ app/src/main/java/com/starry/greenstash/ui/screens/recognition/composables/RecognitionScreen.kt
@@ -34,15 +34,12 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.starry.greenstash.R
-import com.starry.greenstash.ui.screens.emotion.EmotionViewModel
-import com.starry.greenstash.ui.screens.emotion.composables.EmotionViewModelFactory
 import com.starry.greenstash.ui.screens.recognition.RecognitionViewModel
 import com.starry.greenstash.utils.ImageUtils
 
