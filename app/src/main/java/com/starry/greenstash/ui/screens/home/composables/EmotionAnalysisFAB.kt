c457b73475e38ada355c8cae5f3ae44778d1055c
陈子昂 <a960015420@163.com>
2024-11-15 23:09:29 +0800

    refactor(ui): 优化情感分析 FAB 组件
    - 移除了未使用的导入和注释- 删除了未使用的变量和导入
    - 简化了布局结构，提高了代码可读性

--- app/src/main/java/com/starry/greenstash/ui/screens/home/composables/EmotionAnalysisFAB.kt
+++ app/src/main/java/com/starry/greenstash/ui/screens/home/composables/EmotionAnalysisFAB.kt
@@ -1,30 +1,22 @@
 package com.starry.greenstash.ui.screens.home.composables
 
-import android.app.Activity
-import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.SentimentVerySatisfied
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
@@ -43,8 +35,6 @@ fun EmotionAnalysisFAB(
     lazyListState: LazyListState,
     navController: NavController,
 ) {
-//    val errorMessage = stringResource(id = R.string.emotion_analysis_button_error) // 提前获取字符串资源
-
     val isFabVisible = lazyListState.isScrollingUp()
     val density = LocalDensity.current
     val view = LocalView.current
